f8c8d1c07704a3677fb646d1b58fbc72931bed19
hazem nahdi <hazem.nahdi31@gmail.com>
2025-08-27 15:13:52 +0100

    modification des postes

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const navbar = document.getElementById("navbar");
 menuToggle.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
+const typed = new Typed('.typing', {
+      strings: ['Développeur FULLSTACK', 'Technicien Réseaux'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDeploy: 1200,
+      loop: true,
+    });
